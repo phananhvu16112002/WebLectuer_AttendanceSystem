ba398476d84c31229b48d64c0f8b7af3a9e729b5
Phan Anh Vu <phananhvu12a5chilang@gmail.com>
2024-09-06 00:06:33 +0700

    update socket

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Constants{
-  static String? baseURL = 'https://k8sattendancesystem.org'; //34.128.70.247
-  static String? urlForSocket = 'https://backendsystemsocketserver-production.up.railway.app';
+  static String? baseURL = 'https://k8sattendancesystem.org'; //34.128.70.247 https://k8sattendancesystem.org
+  static String? urlForSocket = 'https://backendsystemsocketserver-production-e066.up.railway.app/';
 }
